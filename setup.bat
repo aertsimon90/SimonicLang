6a9a208dfaf1c437b66c1fd3b175e37a7869ad42
aertsimon90 <141768315+aertsimon90@users.noreply.github.com>
2024-02-25 15:39:37 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,2 +1,13 @@
 pip install --upgrade python
 pip install tkinter
+pip install pygame
+pip install openai==0.28
+pip install requests
+pip install opencv-python
+pip install opencv-contrib-python
+pip install pyautogui
+pip install win10toast win32api
+pip install plyer
+pip install Pillow
+pip install autopep8
+pip install fs midiutil flask psutil fake_useragent datetime
