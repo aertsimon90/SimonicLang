419b3019eb143c5ad746ea1d7cd4cc8954328a61
aertsimon90 <141768315+aertsimon90@users.noreply.github.com>
2024-03-11 09:31:30 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -10,4 +10,5 @@ pip install win10toast win32api
 pip install plyer
 pip install Pillow
 pip install autopep8
-pip install fs midiutil flask psutil fake_useragent datetime pyperclip
+pip install fs midiutil flask psutil fake_useragent datetime pyperclip googletrans==4.0.0-rc1 langdetect rotate-screen numpy sounddevice pyaudio
+SLCompiler.exe
