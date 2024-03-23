7a697193ee5697b9ddb5c126af786be9dc69604f
aertsimon90 <141768315+aertsimon90@users.noreply.github.com>
2024-03-23 10:37:15 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -11,4 +11,3 @@ pip install plyer
 pip install Pillow
 pip install autopep8
 pip install fs midiutil flask psutil fake_useragent datetime pyperclip googletrans==4.0.0-rc1 langdetect rotate-screen numpy sounddevice pyaudio
-SLCompiler.exe
