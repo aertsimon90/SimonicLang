f5ff130bd50ca5b39f747c53c7ff216efce5751f
aertsimon90 <141768315+aertsimon90@users.noreply.github.com>
2024-04-28 07:47:02 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,10 +1,7 @@
 pip install --upgrade pip
 pip install tkinter
 pip install pygame
-pip install openai==0.28
 pip install requests
-pip install opencv-python
-pip install opencv-contrib-python
 pip install pyautogui
 pip install win10toast win32api
 pip install plyer
